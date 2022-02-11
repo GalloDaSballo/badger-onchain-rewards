618e86891a526aad2e6fab7bea9e846abf7a8e54
Entreprenerd <valeranijr@gmail.com>
2022-02-11 16:51:27 +0100

    feat: accrue points across epochs

--- tests/unit/test_accrue_points.py
+++ tests/unit/test_accrue_points.py
@@ -43,3 +43,59 @@ def test_full_deposit_one_user(initialized_contract, user, fake_vault):
   assert initialized_contract.totalPoints(EPOCH, fake_vault) == EXPECTED_VAULT_POINTS
 
   assert EXPECTED_POINTS == EXPECTED_VAULT_POINTS
+
+
+## One deposit, total supply is the one deposit
+## Means that at end of epoch
+## My points == total Points
+## If I do the same on epoch 2, I get the same points for epoch 2 as the system brings the totalSupply and epoch after the lookback
+def test_full_deposit_one_user_two_epochs(initialized_contract, user, fake_vault):
+  INITIAL_DEPOSIT = 1e18
+
+  EPOCH = 1
+
+  initialized_contract.notifyTransfer(AddressZero, user, INITIAL_DEPOSIT, {"from": fake_vault})
+
+  epochData = initialized_contract.epochs(EPOCH)
+  difference = epochData[1] - initialized_contract.lastUserAccrueTimestamp(EPOCH, fake_vault, user)
+  vault_difference = epochData[1] - initialized_contract.lastAccruedTimestamp(EPOCH, fake_vault)
+
+  chain.sleep(initialized_contract.SECONDS_PER_EPOCH() + 1)
+  chain.mine()
+
+  assert initialized_contract.points(EPOCH, fake_vault, user) == 0 ## Have yet to accrue
+
+  initialized_contract.accrueUser(EPOCH, fake_vault, user)
+  initialized_contract.accrueVault(EPOCH, fake_vault)
+
+  ## Points is going to be deposit * time per epoch
+  EXPECTED_POINTS = difference * INITIAL_DEPOSIT
+  EXPECTED_VAULT_POINTS = vault_difference * INITIAL_DEPOSIT
+
+  ## NOTE: I've sometimes seen the test fail with zero, I believe this can happen if the evm clock doesn't move
+  ## If you can predictably figure this out, reach out at alex@badger.finance
+  assert approx(initialized_contract.points(EPOCH, fake_vault, user), EXPECTED_POINTS, 1)
+  assert initialized_contract.totalPoints(EPOCH, fake_vault) == EXPECTED_VAULT_POINTS
+
+  assert EXPECTED_POINTS == EXPECTED_VAULT_POINTS
+
+  initialized_contract.startNextEpoch()
+
+  chain.sleep(initialized_contract.SECONDS_PER_EPOCH() + 100000)
+  chain.mine()
+
+  initialized_contract.accrueUser(EPOCH, fake_vault, user)
+  initialized_contract.accrueVault(EPOCH, fake_vault)
+
+  ## Because we never interacted for epoch 2, we expect exact full points
+  EXPECTED_POINTS = INITIAL_DEPOSIT * initialized_contract.SECONDS_PER_EPOCH()
+  EXPECTED_VAULT_POINTS = INITIAL_DEPOSIT * initialized_contract.SECONDS_PER_EPOCH()
+
+  assert approx(initialized_contract.points(EPOCH, fake_vault, user), EXPECTED_POINTS, 1)
+  assert initialized_contract.totalPoints(EPOCH, fake_vault) == EXPECTED_VAULT_POINTS
+
+  assert EXPECTED_POINTS == EXPECTED_VAULT_POINTS
+
+
+
+
