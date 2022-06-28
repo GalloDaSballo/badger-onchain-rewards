f6b8f6c5a5be6b5b6f9d697bedaabaa98ae93a47
Entreprenerd <valeranijr@gmail.com>
2022-06-28 03:11:37 +0200

    refactor: brough vault to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from brownie import RewardsManager, accounts, interface
+from brownie import RewardsManager, accounts, interface, TestVault
 import pytest
 
 
@@ -77,6 +77,24 @@ def setup_contract(deployer):
     ## TODO: Ask for someone to refactor all the INITIAL_DEPOSITS here
 
     return contract
+
+
+"""
+  Extra set of fixtures for integration test with BadgerVaults
+"""
+@pytest.fixture
+def deposit_amount():
+  return 1e18
+
+@pytest.fixture
+def real_vault(token, initialized_contract, deployer, deposit_amount, user):
+  v = TestVault.deploy(token, initialized_contract, {"from": deployer})
+
+  token.approve(v, deposit_amount, {"from": user})
+  v.deposit(deposit_amount, {"from": user})
+
+  return v
+
 
 ## Forces reset before each test
 @pytest.fixture(autouse=True)
