696eee9426f0caf427d1d4062a641938cd13889b
Entreprenerd <alex@entreprenerd.xyz>
2022-08-24 16:55:01 +0200

    feat: resusable claim functions

--- scripts/sims/0_7_emissions_A_B_Bp_with_virtual_accounts.py
+++ scripts/sims/0_7_emissions_A_B_Bp_with_virtual_accounts.py
@@ -55,7 +55,7 @@ Where B and B' are not all for the depositors of A
 """
 
 ## Should we use randomness or use just the values provided?
-DETERMINISTIC = False
+DETERMINISTIC = True
 
 ## NOTE: a 1 epoch test MUST always pass 
 ## because the issue of Future Rewards Backwards Claims is not relevant (there is no epoch of unclaimable rewards)
@@ -77,8 +77,8 @@ VAULT_B_MIN_SELF_EMISSION = 500
 VAULT_B_SELF_EMISSIONS = 1_000_000 ## 100M ETH example - Exacerbates issue with B -> B Claim
 
 ## Additional B Rewards (We call them D to separate)
-VAULT_B_MIN_REWARDS_TO_OTHER = 100
-VAULT_B_REWARDS_TO_OTHER = 100_000 ## Inflates total supply but is not added to rewards
+VAULT_B_MIN_REWARDS_TO_OTHER = 0
+VAULT_B_REWARDS_TO_OTHER = 0 ## Inflates total supply but is not added to rewards
 
 ## NOTE: Unused
 ## NOTE: See Math to prove we don't need as long as we have `VAULT_B_REWARDS_TO_OTHER`
@@ -238,65 +238,10 @@ def multi_claim_sim():
 
 
 
-  ##### CLAIM B ######
-  total_claimed_direct = 0
-
-  for epoch in range(number_of_epochs):
-    divisor = total_points_a ## No subtraction as rewards are from A which is not self-emitting
-
-    for user in range(number_of_users):      
-
-      user_total_rewards_fair = points_a[user] * rewards_b[epoch] // divisor
-      user_total_rewards_dust = points_a[user] * rewards_b[epoch] % divisor
-
-      claimed_points = user_total_rewards_fair * SECONDS_PER_EPOCH
-
-      ## TODO: When claiming A -> B
-      ## Claim B -> B' from previous epochs
-      ## Then use B to claim B -> B' current
-      ## The B -> B' can be done later I think
-
-      ## NOTE:
-      ## user_total_rewards_fair becomes the virtual account
-      ## We simulate recursively the emission claim it performed
-      ## We claim it
-
-      ## If Claim Epoch > 0 (epoch 1 or more)
-      ## Reward from Epoch 1 -> Emissions from epoch 0
-      ## Reward from Epoch 2 -> Emissions from Epoch 0 and Epoch 1, etc..
-      ## Memoized Reward from Epoch N = Memoized Reward from Epoch N - 1
-      ## If Epoch N - 1 = 0 -> Reward(Emission)
-
-      #### MEMOIZED EMISSIONS FOR REWARD
-      ## TODO
-      if (epoch > 0):
-        ## TODO: Create as function for better re-usability
-        process_virtual_account_emissions(user_total_rewards_fair, emissions_b_b, epoch)
-
-        for lookback_epoch in range(epoch):
-          ## Given Reward claimed, capture emission
-          ## Given emission, capture emission of received + 1 epoch
-          
-
-      ## TODO: Process Emissions Here for total Account
-      
-
-      ## Add new rewards to user points_a for next epoch
-      ## Port over old points_a (cumulative) + add the claimed this epoch
-      old_points = points_b[user][epoch - 1] if epoch > 0 else 0
-      points_b[user][epoch] = old_points + claimed_points
-
-      total_claimed_b += user_total_rewards_fair
-      total_dust_b += user_total_rewards_dust
-
-      total_claimed_direct += user_total_rewards_fair
-
-    ## Ensure basic math is correct, all rewards are claimed
-  assert total_claimed_b / total_rewards_b * 100 == 100
-  assert total_rewards_b >= total_claimed_direct ## Check of fairness
 
 
   ###### Claim B from B - B -> B' #######
+  total_claimed_self_emissions_b = 0
 
   total_points_b = total_supply_b * SECONDS_PER_EPOCH
 
@@ -350,49 +295,6 @@ def multi_claim_sim():
   if(len(points_noise_claimed_points) > 1):
     assert points_noise_claimed_points[1] == noise_rewards_b[0] * SECONDS_PER_EPOCH + noise_rewards_b[1] * SECONDS_PER_EPOCH
 
-  ## Actually Claim B -> B'
-
-  ## Accumulator of total emissions claimed to check accuracy of model
-  total_claimed_self_emissions_b = 0
-
-  ## Accumulator of all b_rewards claimed so we can port over to next epoch to simulate compound rewards
-  prev_user_claim_acc = []
-  for user in range(number_of_users):
-    prev_user_claim_acc.append(0)
-
-  for epoch in range(number_of_epochs):
-    divisor = total_points_b - emissions_b_b_points_cumulative_per_epoch[epoch]
-
-    for user in range(number_of_users):
-
-      user_total_rewards_fair = emissions_b_b[epoch] * points_b[user][epoch] // divisor
-      user_total_rewards_dust = emissions_b_b[epoch] * points_b[user][epoch] % divisor
-
-      claimed_points = user_total_rewards_fair * SECONDS_PER_EPOCH
-      ## Add new rewards to user points for next epoch
-      ## Port over old points (cumulative) + add the claimed this epoch
-      points_b[user][epoch] += claimed_points
-
-      ## Compounding accrual
-      prev_user_claim_acc[user] += claimed_points
-
-      # assert prev_user_claim_acc[user] > 0
-
-      if epoch + 1 < number_of_epochs:
-        # Port over cumulative claims
-        points_b[user][epoch + 1] += prev_user_claim_acc[user]
-
-
-      total_claimed_b += user_total_rewards_fair
-      total_claimed_self_emissions_b += user_total_rewards_fair
-      total_dust_b += user_total_rewards_dust
-
-  ## NOTE: Direct B -> B' Claim will fail for now, as you're not getting the many emissions that are being earned by future rewards 
-  
-  if total_emissions_b_b > 0:
-    print_if("Percent of emissions claimed via Direct Claims")
-    print_if(total_claimed_b / total_emissions_b_b * 100)
-
 
   ###### D VIRTUAL ACCOUNTS ######
   total_emissions_claimed_by_noise = 0
@@ -430,6 +332,7 @@ def multi_claim_sim():
 
 
   ###### VIRTUAL ACCOUNTS ######
+  virtual_account_rewards = 0
   ## TODO: Leave Virtual Accounts for D -> B' as we are not claiming that
   ## Treat Future Rewards as if they are accounts, claiming each epoch and using those claims for each subsequent claim
   total_rewards_points_b = total_rewards_b * SECONDS_PER_EPOCH + total_noise_rewards_b * SECONDS_PER_EPOCH## All points for all rewards
@@ -448,34 +351,80 @@ def multi_claim_sim():
     
 
 
-  ## TODO: Figure out if comment / delete or fix
-  ## Math is off because unclaimable get's changed by the other D reward
-  ## First one is equal to all points
-  # assert unclaimable_points_rewards_b_epoch[0] == total_rewards_points_b - rewards_b[0] * SECONDS_PER_EPOCH
-  # ## Last is equal to 0
-  # assert unclaimable_points_rewards_b_epoch[-1] == 0
-  
+  ##### CLAIM B and B' and Virtual Accounts(B -> B') ######
+  total_claimed_direct = 0
 
-  virtual_account_rewards = 0
-  for epoch_index in range(number_of_epochs):
-    total_unclaimed_points = unclaimable_points_rewards_b_epoch[epoch_index] ## Unclaimed are future rewards receiving emissions from previous epochs
+  for epoch in range(number_of_epochs):
+    divisor = total_points_a ## No subtraction as rewards are from A which is not self-emitting
 
-    ## HUNCH: I A -> B, is claiming the rewards
-    ## B -> B' -> Claiming the emissions via the rewards
-    ## THIS: Get the unclaimed rewards, and claim the emissions from them.
-    ## Since we have A -> B and B -> B':
-    ## A -> Works in spite of B -> B'
-    ## While B -> B' requires the B claimable directly and the B' claimed retroactively
+    for user in range(number_of_users):      
 
-    ## Calculate rewards earned for epochs before `epoch_index`
-    virtual_divisor = total_points_b - emissions_b_b_points_cumulative_per_epoch[epoch_index] ## Same as for B -> B'
+      user_total_rewards_fair = points_a[user] * rewards_b[epoch] // divisor
+      user_total_rewards_dust = points_a[user] * rewards_b[epoch] % divisor
 
-    print_if("total_unclaimed_points / divisor")
-    print_if((total_unclaimed_points + virtual_account_rewards * SECONDS_PER_EPOCH) // virtual_divisor * 100)
+      ## TODO: When claiming A -> B
+      ## Claim B -> B' from previous epochs
+      ## Then use B to claim B -> B' current
+      ## The B -> B' can be done later I think
 
-    rewards_earned = emissions_b_b[epoch_index] * (total_unclaimed_points + virtual_account_rewards * SECONDS_PER_EPOCH) // virtual_divisor
+      ## NOTE:
+      ## user_total_rewards_fair becomes the virtual account
+      ## We simulate recursively the emission claim it performed
+      ## We claim it
 
-    virtual_account_rewards += rewards_earned
+      ## If Claim Epoch > 0 (epoch 1 or more)
+      ## Reward from Epoch 1 -> Emissions from epoch 0
+      ## Reward from Epoch 2 -> Emissions from Epoch 0 and Epoch 1, etc..
+      ## Memoized Reward from Epoch N = Memoized Reward from Epoch N - 1
+      ## If Epoch N - 1 = 0 -> Reward(Emission)
+
+      #### MEMOIZED EMISSIONS FOR REWARD
+      current_virtual_reward_earned, current_virtual_reward_dust = process_virtual_account_emissions(
+        user_total_rewards_fair, 
+        total_points_b,
+        emissions_b_b, 
+        emissions_b_b_points_cumulative_per_epoch,
+        epoch
+      )
+
+      virtual_account_rewards += current_virtual_reward_earned
+
+
+      ## TODO: Process Emissions Here for total Account
+      #### PROCESS EMISSIONS FROM TOTAL BALANCE
+      b_rewards_eligible_for_emissions = user_total_rewards_fair + current_virtual_reward_earned
+
+      current_epoch_emissions_earned, current_epoch_emissions_dust = process_emissions_for_epoch(
+        b_rewards_eligible_for_emissions,
+        total_points_b,
+        emissions_b_b,
+        emissions_b_b_points_cumulative_per_epoch,
+        epoch
+      )
+
+      total_claimed_self_emissions_b += current_epoch_emissions_earned
+
+
+
+      ## TODO: CHECK
+      ## Add the rewards + virtual_accounts emissions + emissions claimed this epoch
+      claimed_points = user_total_rewards_fair * SECONDS_PER_EPOCH + current_virtual_reward_earned * SECONDS_PER_EPOCH + current_epoch_emissions_earned * SECONDS_PER_EPOCH
+      
+
+      ## Add new rewards to user points_a for next epoch
+      ## Port over old points_a (cumulative) + add the claimed this epoch
+      old_points = points_b[user][epoch - 1] if epoch > 0 else 0
+      points_b[user][epoch] = old_points + claimed_points
+
+      total_claimed_b += user_total_rewards_fair
+      total_dust_b += user_total_rewards_dust
+
+      total_claimed_direct += user_total_rewards_fair
+
+    ## Ensure basic math is correct, all rewards are claimed
+  assert total_claimed_b / total_rewards_b * 100 == 100
+  assert total_rewards_b >= total_claimed_direct ## Check of fairness
+
 
   ## Use if in case you test with zero-emissions
   if total_emissions_b_b > 0:
@@ -512,3 +461,76 @@ def main():
   print(fair_count)
   print("Overall Percent of passing tests")
   print(fair_count / ROUNDS * 100)
+
+
+
+def process_virtual_account_emissions(user_rewards, total_points_b, emissions_b_b, emissions_b_b_points_cumulative_per_epoch, epoch):
+  """
+    user_rewards -> Rewards the user got at epoch
+    total_points_b -> All points for token b (Math V1 Divisor)
+    emissions_b_b -> All emissions by epoch
+    
+    emissions_b_b_points_cumulative_per_epoch -> Cumulative points of emissions
+        Used to calculate the Circulating Supply from Contract POV
+    epoch -> Current epoch
+    
+    We will calculate rewards for all emissions from epoch 0 to epoch - 1
+  """
+
+  ## We know this because the reward has been sitting in the sim since beginning
+  reward_points = user_rewards * SECONDS_PER_EPOCH
+
+  ## NOTE: For Real Contract first-epoch time will always be less than max, we'll have to deal with the extra math in the impl
+  ##  We can do this wlog because there exists a c for which cT' * S === T * S === T * cS'
+
+  virtual_total_reward = 0
+  virtual_total_dust = 0
+  for old_epoch in range(epoch):
+    divisor = total_points_b - emissions_b_b_points_cumulative_per_epoch[old_epoch]
+
+    user_total_rewards_fair = emissions_b_b[old_epoch] * reward_points // divisor
+    user_total_rewards_dust = emissions_b_b[old_epoch] * reward_points % divisor
+
+    claimed_points = user_total_rewards_fair * SECONDS_PER_EPOCH
+
+
+    ## Add points for next epoch as we simulate full claim
+    reward_points += claimed_points
+
+    virtual_total_reward += user_total_rewards_fair
+    virtual_total_dust += user_total_rewards_dust
+  
+  return (virtual_total_reward, virtual_total_dust)
+
+
+def process_emissions_for_epoch(b_rewards_eligible_for_emissions, total_points_b, emissions_b_b, emissions_b_b_points_cumulative_per_epoch, epoch):
+  """
+    b_rewards_eligible_for_emissions -> Rewards the user got at epoch
+    total_points_b -> All points for token b (Math V1 Divisor)
+    emissions_b_b -> All emissions by epoch
+    
+    emissions_b_b_points_cumulative_per_epoch -> Cumulative points of emissions
+        Used to calculate the Circulating Supply from Contract POV
+    epoch -> Current epoch
+    
+    We will calculate claimable emissions for this epoch only
+  """
+
+    ## We know this because the reward has been sitting in the sim since beginning
+  reward_points = b_rewards_eligible_for_emissions * SECONDS_PER_EPOCH
+
+  ## NOTE: For Real Contract first-epoch time will always be less than max, we'll have to deal with the extra math in the impl
+  ##  We can do this wlog because there exists a c for which cT' * S === T * S === T * cS'
+
+  total_reward = 0
+  total_dust = 0
+
+  divisor = total_points_b - emissions_b_b_points_cumulative_per_epoch[epoch]
+
+  user_total_rewards_fair = emissions_b_b[epoch] * reward_points // divisor
+  user_total_rewards_dust = emissions_b_b[epoch] * reward_points % divisor
+
+  total_reward += user_total_rewards_fair
+  total_dust += user_total_rewards_dust
+  
+  return (total_reward, total_dust)
